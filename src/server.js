e2d0bb5c04ee0c7369b5b518ae9da928cb0c3357
Vinícius Barros <vicaetano_barros@hotmail.com>
2022-10-11 09:15:51 -0300

    feat: server created using express

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express, { json } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(json());
+app.use(cors());
+
+app.listen(process.env.PORT, () => {
+  console.log(`Running on port ${process.env.PORT}`);
+});
